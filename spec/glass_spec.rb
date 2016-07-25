429b6d5eed053c17c3f2129eef447891ada8d94f
Olivia Hinton <hinton.olivia@gmail.com>
2016-07-25 14:38:31 -0700

    add notes for through spec

--- spec/glass_spec.rb
+++ spec/glass_spec.rb
@@ -2,7 +2,7 @@ require("spec_helper")
 
 describe(Glass) do
   describe("#recipes") do
-    it("has a recipes") do
+    it("has many recipes") do
       test_glass = Glass.create({:name => "martini"})
       test_recipe1 = Recipe.create({:name => "Manhattan", :glass_id => test_glass.id})
       test_recipe2 = Recipe.create({:name => "Old Fashion", :glass_id => test_glass.id})
