66ebf079f86e1c52cdcf87fecf0bb456621be645
Olivia Hinton <hinton.olivia@gmail.com>
2016-07-25 16:41:05 -0700

    add passing integration spec to the ingredients list route

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,3 +6,11 @@ describe("the index route", :type => :feature) do
     expect(page).to have_content("Landing")
   end
 end
+
+describe("the ingredients list route", :type => :feature) do
+  it "takes us to the ingredients list page" do
+    visit "/"
+    click_link("Enter")
+    expect(page).to have_content("Choose your ingredients:")
+  end
+end
