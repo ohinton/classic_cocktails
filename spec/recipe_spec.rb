f27793efc61faacba3523139b05ff257dac41db5
Rachael Bermudes <bermudesorama@gmail.com>
2016-07-25 13:53:10 -0700

    create html and crop images for slide effect

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,4 +6,23 @@ describe(Recipe) do
     expect(recipe.save()).to(eq(false))
   end
 
+  it("converts the recipe name to titlecase") do
+    recipe = Recipe.create({:name => "test name"})
+    expect(recipe.name()).to(eq("Test Name"))
+  end
+
+# NEED TO FIGURE OUT HOW TO WRITE THIS SPEC! #
+#   describe("#ingredients") do
+# end
+
+  describe("#glass") do
+    it("has a glass") do
+      test_glass = Glass.create({:name => "martini"})
+      test_recipe = Recipe.create({:name => "Manhattan", :glass_id => test_glass.id})
+      expect(test_recipe.glass()).to(eq(test_glass))
+    end
+  end
+
+
+
 end
