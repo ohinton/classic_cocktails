c638e840c09d7873d2088fe9ae46cca6ec1b218e
Maldon <maldonmeehan@gmail.com>
2016-07-26 15:11:44 -0700

    incorporate html pages into erb pages

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,14 +3,14 @@ require("spec_helper")
 describe("the index route", :type => :feature) do
   it "takes us to the home page" do
     visit "/"
-    expect(page).to have_content("Landing")
+    expect(page).to have_content("Classic Cocktail Companion")
   end
 end
 
 describe("the ingredients list route", :type => :feature) do
   it "takes us to the ingredients list page" do
     visit "/"
-    click_link("Enter")
-    expect(page).to have_content("Choose your ingredients:")
+    click_link("Here")
+    expect(page).to have_content("Choose Your Liqour")
   end
 end
